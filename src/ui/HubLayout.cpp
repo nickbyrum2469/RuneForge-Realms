b5df751536e939b2afc699debf39aba5cbcff8f4
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-01 07:24:26 -0600

    refactor: lay out Frontier home cards and actions

--- src/ui/HubLayout.cpp
+++ src/ui/HubLayout.cpp
@@ -6,10 +6,10 @@ namespace rf::ui {
 
 HubLayout::HubLayout() {
     for (std::size_t i = 0; i < nav.size(); ++i) {
-        nav[i] = Rect{15.0f, 55.0f + static_cast<float>(i) * 82.0f, 140.0f, 66.0f};
+        nav[i] = Rect{15.0f, 64.0f + static_cast<float>(i) * 86.0f, 140.0f, 68.0f};
     }
-    for (std::size_t i = 0; i < cards.size(); ++i) {
-        cards[i] = Rect{195.0f + static_cast<float>(i) * 226.0f, 645.0f, 214.0f, 205.0f};
+    for (std::size_t i = 0; i < featureCards.size(); ++i) {
+        featureCards[i] = Rect{195.0f + static_cast<float>(i) * 276.0f, 645.0f, 255.0f, 205.0f};
     }
 }
 
