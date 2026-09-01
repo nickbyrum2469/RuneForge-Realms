9d1714ca58a7e6749db2c84a404fea6a12eca09f
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-01 16:25:05 -0600

    0.5: make block mining behavior data-driven

--- src/world/blocks/BlockDefinition.h
+++ src/world/blocks/BlockDefinition.h
@@ -11,6 +11,17 @@ enum class ToolClass : std::uint8_t {
     Hand,
     Pickaxe,
     Axe,
+    Shovel,
+};
+
+enum class SoundFamily : std::uint8_t {
+    None,
+    Grass,
+    Dirt,
+    Stone,
+    Wood,
+    Leaves,
+    Water,
 };
 
 struct BlockDefinition {
@@ -20,6 +31,11 @@ struct BlockDefinition {
     bool transparent{true};
     float hardness{0.0f};
     ToolClass preferredTool{ToolClass::Hand};
+    std::uint8_t minimumToolTier{0};
+    float damagePerPreferredStrike{1.0f};
+    float baseStrikeIntervalSeconds{0.45f};
+    float wrongToolEfficiency{0.45f};
+    SoundFamily soundFamily{SoundFamily::None};
     std::uint16_t maxStack{64};
     SurfaceMaterial topMaterial{SurfaceMaterial::Dirt};
     SurfaceMaterial sideMaterial{SurfaceMaterial::Dirt};
