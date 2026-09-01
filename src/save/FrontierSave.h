a400590259f45b84523f20ec010baaee0369ba41
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-01 12:05:07 -0600

    feat(save): extend Frontier save data with micro damage growth mining mode and inventory

--- src/save/FrontierSave.h
+++ src/save/FrontierSave.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "game/Math.h"
+#include "game/inventory/Inventory.h"
+#include "game/mining/MiningMode.h"
 #include "world/WorldEdit.h"
+#include "world/micro/MicroVoxelEdit.h"
 
 #include <cstdint>
 #include <filesystem>
@@ -12,10 +15,14 @@ namespace rf::save {
 
 struct FrontierSaveData {
     std::uint32_t seed{1337};
+    float worldAgeSeconds{};
     game::Vec3 playerPosition{0.5f, 10.0f, 0.5f};
     float yaw{};
     float pitch{-0.08f};
+    game::mining::MiningMode miningMode{game::mining::MiningMode::Mixed};
+    game::inventory::Inventory inventory{};
     std::vector<world::BlockEdit> edits;
+    std::vector<world::micro::MicroVoxelEdit> microEdits;
 };
 
 [[nodiscard]] bool frontierSaveExists(const std::filesystem::path& path) noexcept;
