44f85a811ab7ac4c9c1217b6be00f69f80234602
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-01 11:22:11 -0600

    feat(materials): expand visual material metadata

--- src/render/materials/MaterialDefinition.h
+++ src/render/materials/MaterialDefinition.h
@@ -12,6 +12,9 @@ struct MaterialDefinition {
     float roughness{1.0f};
     float metallic{0.0f};
     float emissive{0.0f};
+    float macroScale{1.0f};
+    float microScale{8.0f};
+    float reliefStrength{0.25f};
     std::string_view detailProfile{"none"};
 };
 
