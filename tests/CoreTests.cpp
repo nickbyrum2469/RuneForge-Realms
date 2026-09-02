f72f315cce0b7fad0f3d4c9a5743e48080b5816c
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-02 14:01:56 -0600

    test: run mesh scheduling regression suite

--- tests/CoreTests.cpp
+++ tests/CoreTests.cpp
@@ -8,6 +8,7 @@ int main() {
     runWorldTests();
     runPersistenceTests();
     runCullingTests();
+    runMeshSchedulingTests();
     runMicroMiningTests();
     runInventoryGrowthTests();
     runDropTests();
