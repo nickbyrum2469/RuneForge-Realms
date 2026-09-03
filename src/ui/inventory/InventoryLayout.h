3c4a4920b05ce643cdbba37ec45514a68b66ab8f
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-02 20:23:19 -0600

    refine: rebalance inventory hierarchy and spacing

--- src/ui/inventory/InventoryLayout.h
+++ src/ui/inventory/InventoryLayout.h
@@ -11,13 +11,13 @@ struct InventoryLayout {
     static constexpr float height = 900.0f;
 
     Rect backdrop{0, 0, width, height};
-    Rect outerFrame{145, 58, 1310, 785};
-    Rect titlePlate{515, 66, 570, 62};
-    Rect equipmentPanel{185, 145, 475, 620};
-    Rect inventoryPanel{700, 145, 715, 620};
-    Rect characterViewport{320, 215, 210, 390};
-    Rect hotbarPanel{360, 780, 880, 74};
-    Rect quickCraftPanel{1010, 205, 350, 170};
+    Rect outerFrame{120, 42, 1360, 820};
+    Rect titlePlate{505, 56, 590, 86};
+    Rect equipmentPanel{165, 165, 480, 595};
+    Rect inventoryPanel{680, 165, 755, 595};
+    Rect characterViewport{320, 245, 205, 335};
+    Rect hotbarPanel{335, 785, 930, 76};
+    Rect quickCraftPanel{1025, 235, 360, 142};
 
     std::array<Rect, 6> equipmentSlots{};
     std::array<Rect, 30> inventorySlots{};
