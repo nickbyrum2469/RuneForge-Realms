3ab8f59e4b5a24467e335860fe86a424e799c802
nickbyrum2469 <nickbyrum2469@gmail.com>
2026-09-02 20:21:42 -0600

    refine: expand RuneForge fantasy UI palette

--- src/ui/theme/RuneForgePalette.h
+++ src/ui/theme/RuneForgePalette.h
@@ -8,17 +8,25 @@ inline D2D1_COLOR_F color(unsigned hex, float alpha = 1.0f) noexcept {
                         static_cast<float>(hex & 0xff) / 255.0f,
                         alpha);
 }
-inline constexpr unsigned Void = 0x04070d;
-inline constexpr unsigned Panel = 0x0a1019;
-inline constexpr unsigned PanelRaised = 0x111b29;
-inline constexpr unsigned BronzeDark = 0x60431f;
-inline constexpr unsigned Bronze = 0xb78436;
-inline constexpr unsigned Gold = 0xe3b85c;
-inline constexpr unsigned GoldBright = 0xffdf82;
-inline constexpr unsigned Ivory = 0xf1ead8;
+inline constexpr unsigned Void = 0x03050a;
+inline constexpr unsigned Backdrop = 0x090b0f;
+inline constexpr unsigned Panel = 0x0d0f12;
+inline constexpr unsigned PanelRaised = 0x17191d;
+inline constexpr unsigned PanelInset = 0x090c11;
+inline constexpr unsigned BronzeDeep = 0x3b260f;
+inline constexpr unsigned BronzeDark = 0x68431b;
+inline constexpr unsigned Bronze = 0xa96d25;
+inline constexpr unsigned GoldDark = 0x9b671f;
+inline constexpr unsigned Gold = 0xd79b38;
+inline constexpr unsigned GoldBright = 0xffcf68;
+inline constexpr unsigned Ivory = 0xf3ead4;
+inline constexpr unsigned Silver = 0xc7c5bf;
 inline constexpr unsigned Muted = 0x9ca6b2;
-inline constexpr unsigned BlueGem = 0x2d8eff;
-inline constexpr unsigned BlueGlow = 0x75c3ff;
+inline constexpr unsigned BlueDeep = 0x074b91;
+inline constexpr unsigned BlueGem = 0x087ee8;
+inline constexpr unsigned BlueGlow = 0x63c7ff;
+inline constexpr unsigned BlueCore = 0xb7eeff;
 inline constexpr unsigned Emerald = 0x4fd175;
+inline constexpr unsigned Danger = 0xd65a48;
 }
 #endif
